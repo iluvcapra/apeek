0fb60c85fa743d9c7eead26da5a9228701384daf
Jamie Hardt <jamiehardt@me.com>
2023-05-19 20:03:41 -0700

    Some refactoring

--- apeek/__init__.py
+++ apeek/__init__.py
@@ -4,16 +4,19 @@ apeek is a package for creating audio waveform previews.
 
 __version__ = "0.1.0"
 
+from typing import List
+
 import numpy as np
 
 from .waveform import WaveformData, WaveformDataSettings, ScalingFactor
 
-
-def rectified_unicode_waveform(data: np.array, height: int = 10) -> str:
-    
+def rectified_unicode_waveform_charmatrix(data: np.array, height: int) -> List[List[str]]:
+    """ 
+    Create a grid of strs for a rectified waveform bargraph
+    """
     BLOCK_CHARS = list("·▁▂▃▄▅▆▇")
     FULL_BLOCK = "█"
-    levels = height * (8)
+    levels = height * (len(BLOCK_CHARS))
 
     accum_array = []
 
@@ -21,17 +24,24 @@ def rectified_unicode_waveform(data: np.array, height: int = 10) -> str:
         rectified_value = max(abs(sample_pair[0]), abs(sample_pair[1]))
         leveled_value = int(rectified_value * levels)
         
-        full, partial = divmod(leveled_value, 8)
+        full, partial = divmod(leveled_value, len(BLOCK_CHARS))
         blank = height - full
 
         accum_array += [list( FULL_BLOCK * full + BLOCK_CHARS[partial] + BLOCK_CHARS[0] * blank)]
+
+    return accum_array
+
+
+def unicode_waveform(data: np.array, height: int) -> str:
+    
+    char_matrix = rectified_unicode_waveform_charmatrix(data, height)
     
     # transpose
     retval_lines = list()
     for j in reversed(range(height)):
         this_line = ""
-        for i in range(len(accum_array)):
-            this_line = this_line + accum_array[i][j]
+        for i in range(len(char_matrix)):
+            this_line = this_line + char_matrix[i][j]
 
         retval_lines += [this_line]
 
